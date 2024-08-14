4c8542dcdf44f519e74f104045f9b335fbeefbab
ShivamH1 <shivamhonra@gmail.com>
2024-08-14 20:12:21 +0530

    Work Component done only left with the transition

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 import { IoIosReturnRight } from "react-icons/io";
 
 function Button() {
   return (
-    <div className='w-40 px-4 py-2 bg-zinc-100 text-black rounded-full flex items-center justify-between'>
-        <span className='text-sm font-medium'>Start a Project</span>
-        <IoIosReturnRight />
+    <div className="w-40 px-4 py-2 bg-zinc-100 text-black rounded-full flex items-center justify-between">
+      <span className="text-sm font-medium">Start a Project</span>
+      <IoIosReturnRight />
     </div>
-  )
+  );
 }
 
-export default Button
+export default Button;
