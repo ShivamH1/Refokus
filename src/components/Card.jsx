84a01b0a20eed68f03e904cc4199fc051e012279
ShivamH1 <shivamhonra@gmail.com>
2024-08-15 01:08:43 +0530

    Added Card Component of Contact Us

---
+++ src/components/Card.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { BsArrowRight } from "react-icons/bs";
+
+function Card({ width, start, para, title, heading, paragraph, hover }) {
+  return (
+    <div className={`w-1/2 bg-zinc-800 p-5 rounded-xl hover:${hover} ${width} min-h-[28rem] flex flex-col justify-between`}>
+      <div className="w-full">
+        <div className="w-full flex justify-between items-center">
+          <h3 className="">{title}</h3>
+          <BsArrowRight />
+        </div>
+        <h1 className="text-3xl font-medium mt-5">{heading}</h1>
+      </div>
+      <div className="down w-full">
+        {start && (
+          <>
+            <h1 className="text-6xl font-semibold tracking-tight leading-none">
+              Start a Project
+            </h1>
+            <button className="rounded-full mt-5 py-2 px-5 border-[1px] border-zinc-50">
+              Contact Us
+            </button>
+          </>
+        )}
+        {para && (
+          <>
+            <p className="text-sm text-zinc-500 font-medium">
+              {paragraph}
+            </p>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/components/Cards.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Card from "./Card";
+
+function Cards() {
+  return (
+    <div className="w-full">
+      <div className="max-w-screen-xl mx-auto py-32 flex gap-1">
+        <Card
+          width={"basis-1/3"}
+          start={false}
+          para={true}
+          title="WORK"
+          heading="Projects & Case Studies"
+          paragraph="Web Design, Webflow Development, and Creative Development."
+        />
+        <Card
+          width={"basis-2/3"}
+          start={true}
+          para={false}
+          title="GET IN TOUCH"
+          heading="Let's get to it together"
+          hover={"bg-violet-600"}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
